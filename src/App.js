9c450fdd52eabcf4ca7b873d605375fd5e7ba7b9
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-08-03 14:46:30 +0000

    setup pages & routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewProduct from './products/pages/NewProduct';
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Users />} />
+      <Route path="/products/new" element={<NewProduct />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/products/pages/NewProduct.jsx
@@ -0,0 +1,5 @@
+const NewProduct = () => {
+  return <h2>NewProduct works</h2>;
+};
+
+export default NewProduct;
